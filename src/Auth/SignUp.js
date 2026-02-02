a2c84db70d6b933464cbb473fe3b10723cca9993
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-02 16:51:10 +0500

    SignUp Screen Form container add

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -1,13 +1,24 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import MyButton from '../components/MyButton';
 
 const SignUp = () => {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require('../assets/bg.png')}
+        source={require('../assets/imgbg.jpg')}
         style={styles.imageBackground}
-      ></ImageBackground>
+      >
+        <Image
+          source={require('../assets/iconlogo.png')}
+          style={styles.headerimage}
+        />
+        <Text style={styles.title}> Commerce App </Text>
+        {/* Form Container */}
+        <View style={styles.formContainer}>
+          <MyButton/>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -16,5 +27,26 @@ export default SignUp;
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  imageBackground: { height: '100%', paddingHorizontal: 20 },
+  imageBackground: {
+    height: '100%',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  headerimage: {
+    height: 50,
+    width: 90,
+    position: 'absolute',
+    right: 20,
+    top: 20,
+  },
+  title: { fontSize: 30, marginTop: 80, fontFamily: 'Audiowide-Regular' },
+  formContainer: {
+    height: 450,
+    width: '100%',
+    backgroundColor: '#fff',
+    borderRadius:20,
+    justifyContent:"center",
+    alignItems:"center",
+    marginTop:30
+  },
 });
