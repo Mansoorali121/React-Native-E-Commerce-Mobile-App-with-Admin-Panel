c4d793abe691cff86d66bd6d5ba733213e20feca
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-10 14:12:18 +0500

    Card Complete

--- src/components/Adminordercard.js
+++ src/components/Adminordercard.js
@@ -6,18 +6,65 @@ const Adminordercard = () => {
     <View style={styles.container}>
       <View style={styles.userOrdercard}>
         <View style={styles.ordertextcontainer}>
-          <Text>
-            Order ID: <Text>#1234</Text>
+          <Text style={{ fontFamily: 'Raleway-Italic-VariableFont_wght' }}>
+            Order ID:{' '}
+            <Text style={{ fontFamily: 'Redressed-Regular' }}>#1234</Text>
           </Text>
-          <Text>
-            Total: <Text>2320</Text>
+          <Text style={{ fontFamily: 'Raleway-Italic-VariableFont_wght' }}>
+            Total: <Text style={{ fontFamily: 'Redressed-Regular' }}>2320</Text>
           </Text>
         </View>
         <View style={styles.ordertextcontainer}>
-          <Text>Ali khan</Text>
-          <Text>
-            Status: <Text>Processing</Text>
+          <Text style={{ fontFamily: 'Audiowide-Regular' }}>Ali khan</Text>
+          <Text style={{ fontFamily: 'Raleway-Italic-VariableFont_wght' }}>
+            Status:{' '}
+            <Text
+              style={{
+                color: 'red',
+                fontWeight: 'bold',
+              }}
+            >
+              Processing
+            </Text>
           </Text>
+        </View>
+        <Text
+          style={{ left: 220, fontFamily: 'NovaFlat-Regular', fontSize: 12 }}
+        >
+          Date: 09 Feb 2026
+        </Text>
+        <View
+          style={{
+            marginHorizontal: 10,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 40,
+          }}
+        >
+          <View
+            style={{
+              height: 25,
+              width: 100,
+              backgroundColor: '#f6f9ff',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderWidth: 1,
+              borderRadius: 5,
+            }}
+          >
+            <Text>View Details</Text>
+          </View>
+          <View style={{
+            height: 25,
+              width: 100,
+              backgroundColor: '#87CEEB',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderWidth: 1,
+              borderRadius: 5,
+          }}>
+            <Text style={{color:"red"}}>Change Status </Text>
+          </View>
         </View>
       </View>
     </View>
