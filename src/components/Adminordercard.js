07b8cef8b707631a6550285bda62002d037dcfe5
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-10 13:53:22 +0500

    Admin order card ui design

--- src/components/Adminordercard.js
+++ src/components/Adminordercard.js
@@ -1,16 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const Adminordercard = () => {
   return (
     <View style={styles.container}>
-      <Text>Adminordercard</Text>
+      <View style={styles.userOrdercard}>
+        <View style={styles.ordertextcontainer}>
+          <Text>
+            Order ID: <Text>#1234</Text>
+          </Text>
+          <Text>
+            Total: <Text>2320</Text>
+          </Text>
+        </View>
+        <View style={styles.ordertextcontainer}>
+          <Text>Ali khan</Text>
+          <Text>
+            Status: <Text>Processing</Text>
+          </Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default Adminordercard;
 
 const styles = StyleSheet.create({
-    container:{marginTop:20},
-})
+  container: { marginTop: 20 },
+  userOrdercard: {
+    backgroundColor: '#f6f9ff',
+    height: 200,
+    width: '100%',
+    borderRadius: 12,
+  },
+  ordertextcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
